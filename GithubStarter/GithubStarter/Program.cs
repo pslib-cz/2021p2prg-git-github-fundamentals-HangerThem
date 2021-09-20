8f227b4e987c29be87bdfb0176a8b13ffdd0fc04
HangerThem <35465244+HangerThem@users.noreply.github.com>
2021-09-20 09:02:40 +0200

    Add files via upload

---
+++ GithubStarter/GithubStarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
